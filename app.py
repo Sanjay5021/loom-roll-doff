fba718e6f89186829c79b2202cfc18bcd326bd34
Sanjay5021 <160238687+Sanjay5021@users.noreply.github.com>
2026-04-03 10:54:00 +0530

    dt.date added

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if "uploaded_file" in st.session_state:
 
     my = df[['LoomNo','loomAlcNo','DcoDate','LoomAlcItemcode',
              'BheemNo','LoomalcMtrs','Issue Qty','completedqty']]
-    my['DcoDate'] = pd.to_datetime(my['DcoDate'])
+    my['DcoDate'] = pd.to_datetime(my['DcoDate']).dt.date
 
     #  Input
     # Get unique LoomNos
